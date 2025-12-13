1e412260d73169d57f7cfa7ddeb5e2335eb48105
J2085isa <alvarezramirezisaias11@gmail.com>
2025-12-13 17:15:51 -0500

    Actualizar Jedi.py

--- python/Jedi.py
+++ python/Jedi.py
@@ -12,3 +12,6 @@ def sacarSable(a, c = 0):
         return c
 
 print(sacarSable(mochila))
+
+    R'nzi$+_ +38#822;_;sop aE  b29484'nzb     "$#@alter adoM18348538    2)(/#+@f   recuencia7.0$+_+#;'jsjd38447+2273viaje6.0gkx   jsdufje 58483)(+$-#3   .0 pens   amientos&($(_+358338avtivacio  n13     9483    codigo2085J  AREFAAMENencriptacio nreprodici   ony ejecucion
+  
